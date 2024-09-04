8484f84e6d9c8fa0a8dc3d55d76d6d5fd75ecc44
pablitohaddad <pablohaddad73@gmail.com>
2024-09-03 21:38:39 -0400

    Setando as propriedades

--- app/src/main/java/br/com/pablohaddad/atividadeavaliativa/MainActivity.java
+++ app/src/main/java/br/com/pablohaddad/atividadeavaliativa/MainActivity.java
@@ -1,6 +1,9 @@
 package br.com.pablohaddad.atividadeavaliativa;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText exemplo1, exemplo2;
+    ProgressBar exemplo3;
+    Button exemplo4, exemplo5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        
+        exemplo1 = findViewById(R.id.edtExemplo1);
+        exemplo2 = findViewById(R.id.edtExemplo2);
+        exemplo3 = findViewById(R.id.pgExemplo3);
+        exemplo4 = findViewById(R.id.btnExemplo4);
+        exemplo5 = findViewById(R.id.btnExemplo5);
+
+
+
     }
 }
