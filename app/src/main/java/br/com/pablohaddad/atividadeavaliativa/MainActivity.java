97427d5d342e2b54cef5318d2f09c3cba66d12bf
pablitohaddad <pablohaddad73@gmail.com>
2024-09-04 07:46:40 -0400

    Respondendo questões até a 15

--- app/src/main/java/br/com/pablohaddad/atividadeavaliativa/MainActivity.java
+++ app/src/main/java/br/com/pablohaddad/atividadeavaliativa/MainActivity.java
@@ -1,6 +1,6 @@
 package br.com.pablohaddad.atividadeavaliativa;
 
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,11 +8,7 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
